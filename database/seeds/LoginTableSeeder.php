2dc435ea4ed2ee2b0e21fd3ea90d8bb4b57df521
Jr Martins <juniorm2488@gmail.com>
2017-04-22 20:27:28 -0300

    Concertando bugs de login

--- database/seeds/LoginTableSeeder.php
+++ database/seeds/LoginTableSeeder.php
@@ -12,7 +12,7 @@ class LoginTableSeeder extends Seeder
     public function run()
     {
         \App\User::truncate();
-        factory(\App\User::class,10)->create();
+        // factory(\App\User::class,10)->create();
 
         \App\User::create([
             'name' => "Júnior Martins",
