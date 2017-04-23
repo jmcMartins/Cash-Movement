d2050ca338326d5abefa3f694a568fc090da7125
Guilherme Mamedio <mamedioguilherme@gmail.com>
2017-04-22 21:50:30 -0300

    usuario admin

--- database/seeds/LoginTableSeeder.php
+++ database/seeds/LoginTableSeeder.php
@@ -15,8 +15,8 @@ class LoginTableSeeder extends Seeder
         // factory(\App\User::class,10)->create();
 
         \App\User::create([
-            'name' => "Júnior Martins",
-            'email' => "juniorm2488@gmail.com",
+            'name' => "Administrador",
+            'email' => "admin@gmail.com",
             'password' => bcrypt("123456789"),
         ]);
     }
